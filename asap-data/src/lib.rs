cdba4948fc51e8be389ed2b1298825f39697362a
ypp <ypp@stat.ubc.ca>
2024-11-25 14:41:25 -0800

    temp

--- asap-data/src/lib.rs
+++ asap-data/src/lib.rs
@@ -2,4 +2,4 @@ pub mod common_io; // common IO functions
 pub mod mtx_io; // mtx triplets
 pub mod sparse_matrix_hdf5; // sparse matrix with hdf5 backend
 pub mod sparse_matrix_zarr; // sparse matrix with zarr backend
-
+pub mod simulate; // helper function for simulation
