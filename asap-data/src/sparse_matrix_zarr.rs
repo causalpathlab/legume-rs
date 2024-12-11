ee2486dd8a7d3ca7d623b23e0772345c1b2b23e9
Yongjin Park <ypp@stat.ubc.ca>
2024-12-10 19:10:45 -0800

    bugfix for buffer scope

--- asap-data/src/sparse_matrix_zarr.rs
+++ asap-data/src/sparse_matrix_zarr.rs
@@ -88,10 +88,10 @@ impl SparseMtxData {
         index_by_row: Option<bool>,
     ) -> anyhow::Result<Self> {
         let mut ret = match backend_file {
-            Some(backend_file) => Self::create_backend_file(backend_file)?,
+            Some(backend_file) => Self::return_backend_file(backend_file)?,
             None => {
                 let backend_file = mtx_file.to_string() + ".zarr";
-                Self::create_backend_file(&backend_file)?
+                Self::return_backend_file(&backend_file)?
             }
         };
 
@@ -142,11 +142,11 @@ impl SparseMtxData {
         index_by_row: Option<bool>,
     ) -> anyhow::Result<Self> {
         let mut ret = match zarr_file {
-            Some(backend_file) => Self::create_backend_file(backend_file)?,
+            Some(backend_file) => Self::return_backend_file(backend_file)?,
             None => {
                 let backend_file = create_temp_dir_file(".zarr")?;
                 let backend_file = backend_file.to_str().expect("to_str failed");
-                Self::create_backend_file(&backend_file)?
+                Self::return_backend_file(&backend_file)?
             }
         };
 
@@ -197,7 +197,7 @@ impl SparseMtxData {
     }
 
     /// Helper function to create a new zarr backend file
-    fn create_backend_file(zarr_file: &str) -> anyhow::Result<Self> {
+    fn return_backend_file(zarr_file: &str) -> anyhow::Result<Self> {
         // dbg!(zarr_file);
         use zarrs::group::GroupBuilder;
         let store = Arc::new(FilesystemStore::new(zarr_file)?);
@@ -214,6 +214,28 @@ impl SparseMtxData {
             by_row_indptr: vec![],
             remapped_rows: None,
         })
+    }
+
+    /// Helper function to create a new zarr backend file
+    fn initialize_backend(&mut self) -> anyhow::Result<()> {
+        use zarrs::group::GroupBuilder;
+
+        self.remove_backend_file()?;
+        let zarr_file = &self.file_name;
+        let store = Arc::new(FilesystemStore::new(zarr_file)?);
+        let root = GroupBuilder::new().build(store.clone(), "/")?;
+        root.store_metadata()?;
+
+        self.store = store.clone();
+        self.file_name = zarr_file.to_string().clone();
+        self.chunk_size = CHUNK_SIZE;
+        self.max_column_name_idx = MAX_COLUMN_NAME_IDX;
+        self.max_row_name_idx = MAX_ROW_NAME_IDX;
+        self.by_column_indptr = vec![];
+        self.by_row_indptr = vec![];
+        self.remapped_rows = None;
+
+        Ok(())
     }
 
     /// Clean up the backend file
@@ -442,31 +464,6 @@ impl SparseMtxData {
         Ok(())
     }
 
-    // // type DynZArray = zarrs::array::Array<dyn ReadableListableStorageTraits>;
-
-    // fn open_csc_triplets(
-    //     &self,
-    // ) -> anyhow::Result<(
-    //     zarrs::array::Array<dyn ReadableListableStorageTraits>,
-    //     zarrs::array::Array<dyn ReadableListableStorageTraits>,
-    //     zarrs::array::Array<dyn ReadableListableStorageTraits>,
-    // )> {
-    //     use zarrs::array::Array as ZArray;
-
-    //     let key = "/by_column/indptr";
-    //     let indptr: ZArray<dyn ReadableListableStorageTraits> =
-    //         ZArray::open(self.store.clone(), key)?;
-    //     // let indptr = indptr.retrieve_array_subset_ndarray::<u64>(&indptr.subset_all())?;
-
-    //     let key = "/by_column/data";
-    //     let data = ZArray::open(self.store.clone(), key)?;
-
-    //     let key = "/by_column/indices";
-    //     let indices = ZArray::open(self.store.clone(), key)?;
-
-    //     Ok((indptr, data, indices))
-    // }
-
     /// Helper function to create a filled 1D array with the given
     /// data type and fill value. This is the most useful function to
     /// create a vector like data.
@@ -683,6 +680,8 @@ impl SparseIo for SparseMtxData {
 
             let (new_ncol, new_nrow) = (old2new_cols.len(), old2new_rows.len());
 
+            println!("new_ncol: {}, new_nrow: {}", new_ncol, new_nrow);
+
             /////////////////////////////////////////////////////////
             // 1. Create remapped Mtx only taking a subset of rows //
             /////////////////////////////////////////////////////////
@@ -694,16 +693,19 @@ impl SparseIo for SparseMtxData {
 
             let mut temp_mtx_file = basename(&backend_file)?;
             temp_mtx_file += "_temp.mtx.gz";
-            let mut buf = open_buf_writer(&temp_mtx_file)?;
 
-            writeln!(buf, "%%MatrixMarket matrix coordinate real general")?;
-            writeln!(buf, "{}\t{}\t{}", new_nrow, new_ncol, nnz)?;
+            {
+                let mut buf = open_buf_writer(&temp_mtx_file)?;
 
-            for old_jj in 0..ncol_data {
-                if let Some(jj) = old2new_cols.get(&old_jj) {
+                println!("mtx file: {}", &temp_mtx_file);
+
+                writeln!(buf, "%%MatrixMarket matrix coordinate real general")?;
+                writeln!(buf, "{}\t{}\t{}", new_nrow, new_ncol, nnz)?;
+
+                for (&old_jj, &jj) in old2new_cols.iter() {
                     let (start, end) = (indptr[old_jj], indptr[old_jj + 1]);
-                    let subset = ArraySubset::new_with_ranges(&[start..end]);
 
+                    let subset = ArraySubset::new_with_ranges(&[start..end]);
                     let data_slice = data.retrieve_array_subset_elements::<f32>(&subset)?;
                     let indices_slice = indices.retrieve_array_subset_elements::<u64>(&subset)?;
 
@@ -715,21 +717,29 @@ impl SparseIo for SparseMtxData {
                         }
                     }
                 }
-            }
-            buf.flush()?;
+                buf.flush()?;
+            } // should kept inside
 
             /////////////////////////////////////
             // 2. Remove previous backend file //
             /////////////////////////////////////
-            remove_file(&backend_file)?;
+            self.remove_backend_file()?;
+
+            dbg!(&backend_file);
 
             ///////////////////////////////
             // 3. populate a new backend //
             ///////////////////////////////
-            let mut ret = Self::from_mtx_file(&temp_mtx_file, Some(&backend_file), Some(true))
-                .expect("failed to create a new backend");
-            ret.register_row_names_vec(&new_row_names);
-            ret.register_column_names_vec(&new_col_names);
+            self.initialize_backend()?;
+
+            // populate data from mtx file for faster CSC
+            self.import_mtx_file_by_col(&temp_mtx_file)?;
+            self.read_column_indptr()?;
+            self.import_mtx_file_by_row(&temp_mtx_file)?;
+            self.read_row_indptr()?;
+
+            self.register_row_names_vec(&new_row_names);
+            self.register_column_names_vec(&new_col_names);
 
             #[cfg(not(debug_assertions))]
             {
