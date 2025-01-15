3bcdc3e3a5396e9e2d409de3af2847ff85540a2c
Yongjin Park <ypp@stat.ubc.ca>
2025-01-14 17:05:41 -0800

    start adding matching parts

--- matrix-util/src/traits.rs
+++ matrix-util/src/traits.rs
@@ -1,3 +1,4 @@
+/// Convert to and from triplets
 pub trait MatTriplets {
     type Mat;
     type Scalar;
@@ -13,6 +14,7 @@ pub trait MatTriplets {
     ) -> anyhow::Result<(usize, usize, Vec<(usize, usize, Self::Scalar)>)>;
 }
 
+/// Normalize or scale columns
 pub trait MatOps {
     type Mat;
     type Scalar;
@@ -23,10 +25,28 @@ pub trait MatOps {
     fn scale_columns(&self) -> Self::Mat;
 }
 
+/// Operations to sample random matrices, only works for
+/// `nalgebra::DMatrix` and `ndarray::Array2`
 pub trait SampleOps {
     type Mat;
     type Scalar;
 
     fn runif(dd: usize, nn: usize) -> Self::Mat;
     fn rnorm(dd: usize, nn: usize) -> Self::Mat;
+}
+
+/// Operations that involves multiple types
+pub trait CompositeOps {
+    type Scalar;
+    type Mat;
+    type Other;
+
+    /// `self[:,col] += other[:,col]`
+    /// * `other` - `CscMatrix`
+    /// * `col` - column index
+    fn add_assign_column(&mut self, other: &Self::Other, col: usize);
+
+    /// `self += other`
+    /// * `other` - `CscMatrix`
+    fn add_assign(&mut self, other: &Self::Other);
 }
