9d7329598539e1dffa5ebf80b7785b14faab22c8
Yongjin Park <ypp@stat.ubc.ca>
2025-05-02 20:40:32 -0700

    save adjusted data

--- asap-embed/src/asap_routines_latent_representation.rs
+++ asap-embed/src/asap_routines_latent_representation.rs
@@ -4,26 +4,17 @@ use crate::asap_visitors::VisitColumnsOps;
 
 use log::info;
 
-// use matrix_param::traits::Inference;
-// use matrix_param::traits::ParamIo;
-// use matrix_util::common_io::{extension, read_lines};
 use matrix_util::dmatrix_rsvd::RSVD;
 use matrix_util::traits::*;
 
-// use crate::asap_collapse_data::CollapsingOps;
-// use crate::asap_random_projection::RandProjOps;
-// use crate::asap_routines_post_process::*;
 use asap_data::sparse_io::*;
 use asap_data::sparse_io_vector::*;
 
 use candle_util::candle_data_loader::*;
 use candle_util::candle_loss_functions as loss_func;
-// use candle_util::candle_model_encoder::*;
-// use candle_util::candle_model_topic::*;
 use candle_util::candle_model_traits::*;
 use candle_util::candle_vae_inference::*;
 
-// use clap::{Parser, ValueEnum};
 use std::sync::{Arc, Mutex};
 
 fn visit_nystrom_proj_columnwise(
