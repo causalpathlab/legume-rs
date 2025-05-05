6412f54038fb4887e0dec49643a0a2d577d479d6
Yongjin Park <ypp@stat.ubc.ca>
2025-05-05 15:01:22 -0700

    clean up matching and euclidean distance

--- asap-embed/src/asap_normalization.rs
+++ asap-embed/src/asap_normalization.rs
@@ -24,38 +24,6 @@ impl AdjustByDivisionOp for CscMat {
     }
 }
 
-/// For each column of the matched matrix
-/// RMSE(j,k) = sqrt( sum_g (target[g,j] - source[g,k])^2 / sum_g 1 )
-pub fn compute_euclidean_distance(
-    source: &CscMat,
-    target: &Mat,
-    matched_position_in_target: &Vec<usize>,
-) -> Vec<f32> {
-    let denom = source.nrows() as f32;
-
-    source
-        .col_iter()
-        .zip(matched_position_in_target.iter())
-        .map(|(src_j, &k)| {
-            // target column/cell
-            let tgt_k = target.column(k);
-            // source column/cell
-            let src_rows = src_j.row_indices();
-            let src_vals = src_j.values();
-
-            let tgt_tot = tgt_k.map(|x| x * x).sum();
-            // to avoid double counting
-            let overlap = src_rows.iter().map(|&i| tgt_k[i] * tgt_k[i]).sum::<f32>();
-            let delta_overlap = src_rows
-                .iter()
-                .zip(src_vals.iter())
-                .map(|(&i, &z)| (z - tgt_k[i]) * (z - tgt_k[i]))
-                .sum::<f32>();
-            ((tgt_tot - overlap + delta_overlap) / denom).sqrt()
-        })
-        .collect::<Vec<f32>>()
-}
-
 /// normalize distance
 pub trait NormalizeDistance: Iterator<Item = f32> + Sized {
     fn exp_sum(self, lambda: f32) -> f32
