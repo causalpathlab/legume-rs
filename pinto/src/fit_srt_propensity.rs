d109a5da617ee0da8df85b347c93917eabeeaf84
YPP <ypp@stat.ubc.ca>
2025-09-27 08:44:07 -0700

    exclude row types; gene summary

--- pinto/src/fit_srt_propensity.rs
+++ pinto/src/fit_srt_propensity.rs
@@ -1,6 +1,10 @@
 use crate::srt_common::*;
 use crate::srt_input::read_expr_data;
 use clap::Parser;
+use dmatrix_gamma::GammaMatrix;
+use matrix_param::dmatrix_gamma;
+use matrix_param::io::ParamIo;
+use matrix_param::traits::TwoStatParam;
 use rayon::prelude::*;
 
 #[derive(Parser, Debug, Clone)]
@@ -34,6 +38,10 @@ pub struct SrtPropensityArgs {
     /// coordinate right name
     #[arg(long, default_value = "right_cell")]
     right_name: Box<str>,
+
+    /// Block_size for parallel processing
+    #[arg(long, default_value_t = 100)]
+    block_size: usize,
 
     /// Output header
     #[arg(long, short, required = true)]
@@ -119,26 +127,52 @@ pub fn fit_srt_propensity(args: &SrtPropensityArgs) -> anyhow::Result<()> {
         let genes = data_vec.row_names()?;
         let data_vertices = data_vec.column_names()?;
 
-        // matched_indices
-        let mut stat = Mat::zeros(num_clusters, genes.len());
+        let jobs = matrix_util::utils::generate_minibatch_intervals(
+            data_vec.num_columns()?,
+            args.block_size,
+        );
 
-        // let lb = 0;
-        // let ub = 100;
-        // let x_dn = data_vec.read_columns_csc(lb..ub)?;
+        let partial_stats = jobs
+            .par_iter()
+            .progress_count(jobs.len() as u64)
+            .map(|&(lb, ub)| -> anyhow::Result<(Mat, DVec)> {
+                let x_dn = data_vec.read_columns_csc(lb..ub)?;
+                let mut p_kn = Mat::zeros(prop_kn.nrows(), x_dn.ncols());
 
-	// this is from the prop_kn
-	// data_vertices[lb..ub].iter().map(f)
+                for (i, v) in data_vertices[lb..ub].iter().enumerate() {
+                    if let Some(j) = vertex_index.get(v).map(|x| x.value().clone()) {
+                        p_kn.column_mut(i).copy_from(&prop_kn.column(j));
+                    }
+                }
 
-	// otherwise zeros
+                let n_k = p_kn.column_sum();
 
-        // let matched_indices: Vec<usize> = data_vertices
-        //     .iter()
-        //     .filter_map(|v| vertex_index.get(v).map(|x| x.value().clone()))
-        //     .collect();
+                let sum_dk = x_dn * p_kn.transpose();
 
+                Ok((sum_dk, n_k))
+            })
+            .collect::<anyhow::Result<Vec<_>>>()?;
 
-        // matched_indices.map(|j| {
-        // });
+        let mut sum_dk = Mat::zeros(genes.len(), prop_kn.nrows());
+        let mut n_1k = Mat::zeros(1, prop_kn.nrows());
+
+        for (s_dk, n_k1) in partial_stats {
+            sum_dk += s_dk;
+            n_1k += n_k1.transpose();
+        }
+
+        let mut gamma_param = GammaMatrix::new((sum_dk.nrows(), sum_dk.ncols()), 1.0, 1.0);
+
+        let denom_dk = DVec::from_element(sum_dk.nrows(), 1.0) * n_1k;
+
+        gamma_param.update_stat(&sum_dk, &denom_dk);
+        gamma_param.calibrate();
+
+        gamma_param.to_parquet(
+            Some(genes.as_ref()),
+            None,
+            &(args.out.to_string() + ".genes.parquet"),
+        )?;
     }
 
     info!("Done");
