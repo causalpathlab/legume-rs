ec93d17d92641b82cdc939c4084055fbdf2ca849
Yongjin Park <ypp@stat.ubc.ca>
2025-10-31 06:37:10 -0700

    skeleton code for chickpea

---
+++ chickpea/src/common.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code)]
+
+pub use log::info;
+pub use std::sync::{Arc, Mutex};
+
+pub const DEFAULT_KNN: usize = 10;
+pub const DEFAULT_OPT_ITER: usize = 100;
+
+pub type Mat = nalgebra::DMatrix<f32>;
+pub type DVec = nalgebra::DVector<f32>;
+pub type CscMat = nalgebra_sparse::CscMatrix<f32>;
+
+pub type SparseData = dyn SparseIo<IndexIter = Vec<usize>>;
+
+pub use data_beans::sparse_data_visitors::*;
+pub use data_beans::sparse_io::*;
+pub use data_beans::sparse_io_vector::*;
+
+pub use candle_util::{candle_core, candle_nn};
+
+pub use clap::{Args, Parser, Subcommand, ValueEnum};
+
+pub use matrix_param::io::ParamIo;
+pub use matrix_param::traits::{Inference, TwoStatParam};
+pub use matrix_util::common_io::{remove_file, write_types};
+pub use matrix_util::dmatrix_util::row_membership_matrix;
+pub use matrix_util::traits::*;
+
+pub use data_beans_alg::collapse_data::*;
+pub use data_beans_alg::random_projection::*;
+
---
+++ chickpea/src/input.rs
@@ -0,0 +1,14 @@
+
+
+// 1. build sparse_io_vector for expression
+
+// 2. build sparse_io_vector for atac peaks
+
+// 3. match barcode positions
+
+// 4. loading genomic position information?
+// - gff/gtf?
+
+// cell type assignment analysis?
+// 
+
