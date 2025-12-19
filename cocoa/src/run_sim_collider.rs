6ae76f63f97e8c36e8de7962624b37ae5c235ad7
Yongjin Park <ypp@stat.ubc.ca>
2025-12-19 13:31:34 -0800

    refactor simulation with one type; need to work on many types

--- cocoa/src/run_sim_collider.rs
+++ cocoa/src/run_sim_collider.rs
@@ -1,6 +1,6 @@
 use crate::common::*;
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 pub struct SimColliderArgs {
     #[arg(
         short = 'r',
@@ -17,9 +17,59 @@ pub struct SimColliderArgs {
         long_help = "Number of columns/cells"
     )]
     n_cells: usize,
+
+    #[arg(
+        long,
+        value_delimiter = ',',
+        default_value = "1.0,1.0",
+        help = "hyperparameter for gamma distribution",
+        long_help = "Hyperparameter for gamma distribution"
+    )]
+    gamma_hyperparam: Vec<f32>,
+
+    #[arg(
+        long,
+        default_value_t = 42,
+        help = "random seed",
+        long_help = "Random seed"
+    )]
+    rseed: u64,
+
+    #[arg(
+        long,
+        value_enum,
+        default_value = "zarr",
+        help = "backend",
+        long_help = "Backend"
+    )]
+    backend: SparseIoBackend,
+
+    #[arg(
+        long,
+        default_value_t = false,
+        help = "save mtx",
+        long_help = "Save mtx"
+    )]
+    save_mtx: bool,
+
+    #[arg(
+        long,
+        short,
+        required = true,
+        help = "Output header",
+        long_help = "Output header"
+    )]
+    out: Box<str>,
+
+    #[arg(long, short, help = "verbosity", long_help = "Set RUST_LOG=`info`")]
+    verbose: bool,
 }
 
-fn run_sim_collider_data(args: &SimColliderArgs) -> anyhow::Result<()> {
+pub fn run_sim_collider_data(args: SimColliderArgs) -> anyhow::Result<()> {
+    if args.verbose {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
 
     unimplemented!("");
 
